e7a587c571664813fa586e01de2da5acb6170d65
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-18 02:00:56 -0400

    3 of 10

--- program.js
+++ program.js
@@ -2,8 +2,18 @@
 //console.log("HELLO ES6");
 
 //2 of 10
-const name = process.argv[2];
-let string = `Hello, ${name}!
-Your name lowercased is "${name.toLowerCase()}".`;
+// const name = process.argv[2];
+// let string = `Hello, ${name}!
+// Your name lowercased is "${name.toLowerCase()}".`;
 
-console.log(string);
+// console.log(string);
+
+//3 of 10
+const inputs = process.argv.slice(2);
+
+const reducer = (accu, currVal) => accu + currVal;
+const mapper = (element) => element.substring(0, 1);
+
+let result = inputs.map(mapper)
+    .reduce(reducer);
+console.log('[' + inputs.join(',') + ']' + " becomes \"" + result + "\"");
