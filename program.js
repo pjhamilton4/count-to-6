dc94c14be06a34148cd4145a471088070ec12f47
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-17 23:47:27 -0400

    1 of 10 and 2 of 10

---
+++ program.js
@@ -0,0 +1,9 @@
+//1 of 10
+//console.log("HELLO ES6");
+
+//2 of 10
+const name = process.argv[2];
+let string = `Hello, ${name}!
+Your name lowercased is "${name.toLowerCase()}".`;
+
+console.log(string);
