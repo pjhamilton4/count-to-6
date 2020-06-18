3351a6ef27e8162bd1a7c901911b87c6c40235e6
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-18 14:37:40 -0400

    10 of 10

--- program.js
+++ program.js
@@ -59,5 +59,23 @@
 // }
 
 //9 of 10
-module.exports = (str, num = str.length) => `${str}${'!'.repeat(num)}`;
+// module.exports = (str, num = str.length) => `${str}${'!'.repeat(num)}`;
 
+//10 of 10
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(tpls, ...args) {
+    var str = tpls[0];
+    for (var i = 0; i < args.length; i++) {
+        str += scrub(args[i]) + tpls[i + 1];
+    };
+    return str;
+}
+
+function scrub(text) {
+    return text.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/'/g, "&apos;")
+        .replace(/"/g, "&quot;");
+};
